922e27dde808622a7c9760983c63f18246cb26c4
Paulo Victor LS <34511811+paulovictorls@users.noreply.github.com>
2021-12-03 08:23:47 -0300

    [update] cars and houses
    Update and organization of scraper notebooks

--- test.py
+++
@@ -1,7 +0,0 @@
-import time
-from tqdm import tqdm
-
-for i in tqdm(range(10), desc='Pages', colour='red'):
-    time.sleep(1)
-    for j in tqdm(range(10), desc='Ads', colour='blue'):
-        time.sleep(1)
